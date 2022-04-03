300ec0bb16039a7ca2a53fcd2899506b4ef6f369
Raghav-Prakash <raghrav2995@gmail.com>
2022-04-02 23:16:26 -0600

    Add corect/incorrect background colors to a selected option

--- src/app/questions/question/question.component.ts
+++ src/app/questions/question/question.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Question } from 'src/app/models/question';
-
 @Component({
   selector: 'question',
   templateUrl: './question.component.html',
@@ -11,12 +10,19 @@ export class QuestionComponent implements OnInit {
 
   @Input() question: Question;
   options: string[];
+  selectedAnswerIndex: number;
+  isAnswerCorrect: boolean;
 
   constructor() { }
 
   ngOnInit() {
     this.question.question = this.decodeText(this.question.question);
     this.options = this.getOptions(this.question.incorrectAnswers, this.question.correctAnswer);
+  }
+
+  onSelectOption(option: string) {
+    this.selectedAnswerIndex = this.options.findIndex(opt => opt === option);
+    this.isAnswerCorrect = this.question.correctAnswer === option;
   }
 
   private decodeText(text: string): string {
